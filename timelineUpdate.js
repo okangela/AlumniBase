b1335f7ac30face2a56db7b87c4aea2dbc404c56
Ridhima Gupta <ridhimagupta@gatech.edu>
2016-04-25 11:23:50 -0400

    Added hide and show interactions

--- timelineUpdate.js
+++ timelineUpdate.js
@@ -1,41 +1,47 @@
 function updateTimeline() {
-    d3.json("google.json", function(timeLineData) {
-        // console.log (timeLineData);
-        function timelineStackedIcons() {
-            var chart = d3.timeline()
-                .showTimeAxisTick() // toggles tick marks
-                .colors(jobScale)
-                .colorProperty("job")
-                .stack() // toggles graph stacking
-                .margin({
-                    left: 10,
-                    right: 50,
-                    top: 0,
-                    bottom: 0
-                })
-                .itemHeight(24)
-                .itemMargin(6)
-                .tickFormat({
-                    format: d3.time.format("%Y"),
-                    tickTime: d3.time.year,
-                    tickInterval: 1,
-                    tickSize: 6
-                })
-                .background("none")
-                .orient("top");
+  d3.json("google.json", function(timeLineData) {
+    // console.log (timeLineData);
+    function timelineStackedIcons() {
+      var chart = d3.timeline()
+        .showTimeAxisTick() // toggles tick marks
+        .colors(jobScale)
+        .colorProperty("job")
+        .stack() // toggles graph stacking
+        .margin({
+          left: 10,
+          right: 50,
+          top: 0,
+          bottom: 0
+        })
+        .itemHeight(24)
+        .itemMargin(6)
+        .tickFormat({
+          format: d3.time.format("%Y"),
+          tickTime: d3.time.year,
+          tickInterval: 1,
+          tickSize: 6
+        })
+        .background("none")
+        .orient("top");
 
-            chart.mouseover(function(d, i, datum) {
-                timelineTip.show(d);
-            })
-                .mouseout(function(d, i, datum) {
-                    timelineTip.hide(d);
-                });
+      chart.mouseover(function(d, i, datum) {
+        timelineTip.show(d);
+      })
+        .mouseout(function(d, i, datum) {
+          timelineTip.hide(d);
+        });
 
-            d3.select(".timeline").select("svg").remove();
-            var svg = d3.select(".timeline").append("svg").attr("width", widthTimeline)
-                .datum(timeLineData).call(chart);
-        }
-        timelineStackedIcons();
-    });
+      d3.select(".timeline").select("svg").remove();
+      var svg = d3.select(".timeline").append("svg").attr("width", widthTimeline)
+        .datum(timeLineData).call(chart);
+    }
+    timelineStackedIcons();
+    d3.select(".timeline").style("visibility", "visible");
+    if (filterCount >= 1) {
+      d3.select(".timeline_initial").style("display", "none");
+      d3.select(".timeline").style("visibility", "visible");
+    }
+
+  });
 
 }
